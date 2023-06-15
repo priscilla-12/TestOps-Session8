66eff7312f8ab4a8a523a289652138ae1687506b
priscilla.d <priscilla.d@inbl01lap5844.brd.g-c-i.com>
2023-06-15 15:00:54 +0530

    Commit 2

---
+++ Test Suites/Test Suite 2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>236591ec-1f43-485d-a8f8-b6fcc27d0934</testSuiteGuid>
+</TestSuiteEntity>
